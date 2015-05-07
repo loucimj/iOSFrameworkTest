81a523df7f1cd42f68dc110e3db05cf45d5d3e42
Javier <javier.loucim@gmail.com>
2015-05-07 15:05:19 -0300

    Initial version

---
+++ iOSFrameworkTest/Greetings.h
@@ -0,0 +1,15 @@
+//
+//  Greetings.h
+//  iOSFrameworkTest
+//
+//  Created by Javi on 7/5/15.
+//  Copyright (c) 2015 Javier Loucim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Greetings : NSObject
+
+-(void) helloWorld ;
+
+@end
---
+++ iOSFrameworkTest/Greetings.m
@@ -0,0 +1,18 @@
+//
+//  Greetings.m
+//  iOSFrameworkTest
+//
+//  Created by Javi on 7/5/15.
+//  Copyright (c) 2015 Javier Loucim. All rights reserved.
+//
+
+#import "Greetings.h"
+
+@implementation Greetings
+
+
+-(void) helloWorld {
+    NSLog(@"Hello World");
+}
+
+@end
