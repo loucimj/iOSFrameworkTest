bdbc50a5208309f5cbc533836aded4f554a9869d
Javier <javier.loucim@gmail.com>
2015-05-07 14:40:43 -0300

    Initial Commit

---
+++ iOSFrameworkTest/iOSFrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  iOSFrameworkTest.h
+//  iOSFrameworkTest
+//
+//  Created by Javi on 7/5/15.
+//  Copyright (c) 2015 Javier Loucim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSFrameworkTest.
+FOUNDATION_EXPORT double iOSFrameworkTestVersionNumber;
+
+//! Project version string for iOSFrameworkTest.
+FOUNDATION_EXPORT const unsigned char iOSFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSFrameworkTest/PublicHeader.h>
+
+
